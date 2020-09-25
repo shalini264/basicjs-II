71dc40cca28ec7ec1bfd0396c6e70ed7f924c141
shalini sharma <48730336+shalini264@users.noreply.github.com>
2020-09-25 21:34:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,14 @@ const center = document.querySelector(".center");
 const getBGColor=(num)=>{
     return window.getComputedStyle(num).backgroundColor;
 }
-var pink_color=getBGColor(pink);
-pink.addEventListener("mouseenter",()=>{
-center.style.background=pink_color;
-})
-var red_color=getBGColor(red);
-red.addEventListener("mouseenter",()=>{
-    center.style.background=red_color;
-})
-var blue_color=getBGColor(blue);
-blue.addEventListener("mouseenter",()=>{
-    center.style.background=blue_color;
-})
-var orange_color=getBGColor(orange);
-orange.addEventListener("mouseenter",()=>{
-    center.style.background=orange_color;
-})
+const getColor=(sample,color)=>{
+    return sample.addEventListener("mouseenter",()=>{
+        center.style.background=color;});
+    };
+
+
+getColor(red,getBGColor(red));
+getColor(pink,getBGColor(pink));
+getColor(blue,getBGColor(blue));
+getColor(orange,getBGColor(orange));
+
